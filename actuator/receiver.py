b28f21efea10807dfe0210b92cfd8823f2c5913a
Geesing <chewgeesing@gmail.com>
2025-04-05 18:03:33 -0400

    Loop music and change based on MQTT message

--- actuator/receiver.py
+++ actuator/receiver.py
@@ -3,6 +3,9 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
+import pygame
+import time
+
 # Constants
 SLEEP_TIME = 5
 QOS = 0
@@ -19,30 +22,70 @@ USERNAME = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
 API_KEY = os.getenv("WEATHER_API_KEY")
 
-# Subscribe to topic "emp/weather"
-def on_connect(client, userdata, flags, reason_code, properties):
-    if reason_code == 0:
-        print(f'Connected to {BROKER} successfully.')
-        # Subscribe to the topic
-        client.subscribe(TOPIC, qos=QOS)
-        print(f'Subscribed to {TOPIC}')
-    else:
-        print(f'Connection to {BROKER} failed. Return code={reason_code}')
+class WeatherReceiver:
+    def __init__(self):
+        self.precipitation_state = None
+        self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+        if BROKER_AUTHENTICATION:
+            self.client.username_pw_set(USERNAME, password=PASSWORD)
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+        self.client.connect(BROKER, PORT, KEEPALIVE)
 
-def on_message(client, userdata, msg):
-    print(f"Received message: {msg.payload.decode()}")
+    def on_connect(self, client, userdata, flags, reason_code, properties=None):
+        if reason_code == 0:
+            print(f'Connected to {BROKER} successfully.')
+            client.subscribe(TOPIC, qos=QOS)
+            print(f'Subscribed to {TOPIC}')
+        else:
+            print(f'Connection to {BROKER} failed. Return code={reason_code}')
 
-client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-if BROKER_AUTHENTICATION:
-    client.username_pw_set(USERNAME,password=PASSWORD)
-client.on_connect = on_connect
-client.on_message = on_message
-client.connect(BROKER, PORT, KEEPALIVE)
+    def on_message(self, client, userdata, msg):
+        # In seconds
+        FADE_OUT_TIME = 5
+        FADE_IN_TIME = 1
 
-try:
-    client.loop_forever()
-    pause()
-except KeyboardInterrupt:
-    client.disconnect()
-    print('Done')
+        print(f"Received message: {msg.payload.decode()}")
+        current_precipitation = msg.payload.decode()
+
+        # Initialize pygame mixer if not already initialized
+        if pygame.mixer.get_init() is None:
+            pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=4096)
+
+        # If the precipitation state has changed, change the music
+        if self.precipitation_state != current_precipitation:
+            self.precipitation_state = current_precipitation
+
+            # Fade out any currently playing music
+            if pygame.mixer.music.get_busy():
+                pygame.mixer.music.fadeout(FADE_OUT_TIME * 1000)
+                time.sleep(FADE_OUT_TIME + 1)
+            
+            # Load and play the new track
+            pygame.mixer.music.load(f"./music/{self.get_weather_track()}")
+            pygame.mixer.music.play(loops=-1, fade_ms=FADE_IN_TIME * 1000)
+
+    def get_weather_track(self):
+        if self.precipitation_state in ["rain", "hail", "drizzle"]:
+            return "day_rainy.mp3"
+        elif self.precipitation_state == "snow":
+            return "day_snowy.mp3"
+        elif self.precipitation_state == "none":
+            return "day_sunny.mp3"
+        else:
+            print("Unknown weather condition. Defaulting to day_sunny...")
+            return "day_sunny.mp3"
+
+
+    def start(self):
+        try:
+            self.client.loop_forever()
+        except KeyboardInterrupt:
+            self.client.disconnect()
+            print('Done')
+
+
+# Initialize and start the WeatherReceiver
+receiver = WeatherReceiver()
+receiver.start()
 
